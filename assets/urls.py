7c4910d0f5b70de5740102f7781032dcef38e1bb
Abdullahi <abdullahiabdulwasiu1@gmail.com>
2023-03-03 22:04:33 +0100

    Add Update & Delete Endpoints

--- assets/urls.py
+++ assets/urls.py
@@ -4,8 +4,10 @@ from . import views
 urlpatterns = [
     path('category/', views.ListCreateCategoryAPIView.as_view(), name="create-category"),
     path('asset/company/<str:company_name>/', views.CreateAssetsAPIView.as_view(),
-          name='list-create-assets'),
+          name='create-assets'),
     path('asset/all/', views.ListAssetsAPIView.as_view(), name='list-assets'),
+    path('asset/update/<int:pk>', views.UpdateAssetsAPIView.as_view(), name='update-assets'),
+    path('asset/delete/<int:pk>', views.DeleteAssetsAPIView.as_view(), name='delet-asset'),
     path('assetlog/<int:pk>/', views.CreateAssetLogsAPIView.as_view(),
           name='create-assets-log'),
     path('assetlog/all/', views.ListAssetLogsAPIView.as_view(), name='list-assets'),
--- assets/views.py
+++ assets/views.py
@@ -2,18 +2,20 @@ from django.shortcuts import render
 from .models import Assets, AssetLogs, Category
 from company.models import Company
 from .serializers import AssetsSerializer, AssetLogSerializer, CategorySerializer
-from rest_framework import generics, exceptions, permissions
+from rest_framework import generics, exceptions, permissions, status
+from rest_framework.response import Response
 
-
+# Asset Category
 class ListCreateCategoryAPIView(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
 
 
+# Asset 
 class CreateAssetsAPIView(generics.CreateAPIView):
     serializer_class = AssetsSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAdminUser]
 
     def get_queryset(self):
         try:
@@ -34,6 +36,31 @@ class ListAssetsAPIView(generics.ListAPIView):
     queryset = Assets.objects.all()
 
 
+class UpdateAssetsAPIView(generics.UpdateAPIView):
+   serializer_class = AssetsSerializer
+   permission_classes = [permissions.IsAdminUser]
+
+   def  get_queryset(self):
+      asset_id = Assets.objects.get(id=self.kwargs["pk"])
+      return Assets.objects.filter(id=asset_id.id)
+
+
+class DeleteAssetsAPIView(generics.RetrieveDestroyAPIView):
+   serializer_class =AssetsSerializer
+   permissions_classes = [permissions.IsAdminUser]
+     
+   def get_queryset(self):  
+       asset_id = Assets.objects.get(id=self.kwargs["pk"])
+       return Assets.objects.filter(id=asset_id.id)
+   
+   def delete(self, request, *args, **kwargs):
+        if self.get_queryset().exists():
+            self.get_queryset().delete()
+            return Response( {"success": "Deleted Succesfully"}, status=status.HTTP_204_NO_CONTENT)
+       
+
+
+# Asset Log
 class CreateAssetLogsAPIView(generics.CreateAPIView):
     serializer_class = AssetLogSerializer
     permission_classes = [permissions.IsAuthenticated]
@@ -57,3 +84,6 @@ class ListAssetLogsAPIView(generics.ListAPIView):
     permission_classes = [permissions.AllowAny]
     serializer_class = AssetLogSerializer
     queryset = AssetLogs.objects.all()
+
+
+
